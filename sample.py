ee37017e260a802eb83a6ac4477bbf149aac4957
James Ball <jhb119@ic.ac.uk>
2023-02-08 11:50:49 +0000

    Add sampling with 3 unets

--- sample.py
+++ sample.py
@@ -10,10 +10,43 @@ from train import unet_generator, init_imagen
 
 from patient_dataset import PatientDataset
 import os
+import gc
 import pandas as pd
 from glob import glob
 
 import re
+
+def generate_images(unet_number, args, lowres_images=None):
+    imagen = init_imagen(unet_number)
+    trainer = ImagenTrainer(imagen=imagen)
+
+    if unet_number == 1:
+        path = args.unet1_checkpoint
+    elif unet_number == 2:
+        path = args.unet2_checkpoint
+    else:
+        path = args.unet3_checkpoint
+
+    trainer.load(path)
+
+    conds = torch.tensor([0.0, 0.5, 0.2]).reshape(1, 1, 3).repeat_interleave(args.num_images, dim=0).float().cuda()
+
+    images = trainer.sample(
+        batch_size=args.num_images,
+        return_pil_images=(unet_number==3),
+        text_embeds=conds,
+        start_image_or_video=lowres_images,
+        start_at_unet_number=unet_number,
+        stop_at_unet_number=unet_number,
+    )
+
+    del trainer
+    del imagen
+    del conds
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    return images
 
 
 def main():
@@ -24,38 +57,9 @@ def main():
     except FileExistsError:
         pass
 
-    unet1_imagen = init_imagen(1)
-    unet2_imagen = init_imagen(2)
-
-    unet1_trainer = ImagenTrainer(
-        imagen=unet1_imagen,
-    )
-
-    unet2_trainer = ImagenTrainer(
-        imagen=unet2_imagen,
-    )
-
-    unet1_trainer.load(args.unet1_checkpoint)
-    unet2_trainer.load(args.unet2_checkpoint)
-
-    conds = torch.tensor([0.0, 0.5, 0.2]).reshape(1, 1, 3).repeat_interleave(args.num_images, dim=0).float().cuda()
-
-    lowres_images = unet1_trainer.sample(
-        batch_size=args.num_images,
-        return_pil_images=False,
-        text_embeds=conds,
-        start_at_unet_number=1,
-        stop_at_unet_number=1,
-    )
-
-    highres_images = unet2_trainer.sample(
-        batch_size=args.num_images,
-        return_pil_images=True,
-        text_embeds=conds,
-        start_image_or_video=lowres_images,
-        start_at_unet_number=2,
-        stop_at_unet_number=2,
-    )
+    lowres_images = generate_images(1, args)
+    medres_images = generate_images(2, args, lowres_images=lowres_images)
+    highres_images = generate_images(3, args, lowres_images=medres_images)
 
     for image in highres_images:
         image.save(f'samples/inference-{uuid4()}.png')
@@ -65,6 +69,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--unet1_checkpoint', type=str, default='./unet1_checkpoint.pt', help='Path to checkpoint for unet1 model')
     parser.add_argument('--unet2_checkpoint', type=str, default='./unet2_checkpoint.pt', help='Path to checkpoint for unet2 model')
+    parser.add_argument('--unet3_checkpoint', type=str, default='./unet3_checkpoint.pt', help='Path to checkpoint for unet3 model')
     parser.add_argument('--num_images', type=int, default=1, help='Number of images to generate')
     return parser.parse_args()
 
