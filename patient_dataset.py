d063ff889113fd697e442004fd932f08fa675977
scechnicka <sc7718@ic.ac.uk>
2023-02-19 22:30:22 +0000

    First commit to Kumar dataset pipeline

--- patient_dataset.py
+++ patient_dataset.py
@@ -1,211 +1,69 @@
-import h5py
+import os
 import pandas as pd
 import torch
 from matplotlib import pyplot as plt
 from torch.utils.data import Dataset
-import slideio
 from tqdm import tqdm
 from skimage import color
 import numpy as np
+import torchvision.transforms as T
 
 NUM_FLIPS_ROTATIONS = 8
-NUM_TRANSLATIONS =  4
-
-# Possible kidney outcomes, ordered by severity. DWFG is ignored here as it does not indicate a poor outcome.
-OUTCOMES = ["Functioning", "25%", "50%", "Graft_Loss", "DWGL"]
-
-
-def normalize_patient_outcomes(x):
-    return OUTCOMES.index(x) / len(OUTCOMES) if x in OUTCOMES else 0
-
-
-def normalize_time_post_transplant(x):
-    return (x - 90) / 365
-
-
-def normalize_creatinine(x):
-    return (x - 30) / 2050
-
+NUM_RANDOMCROPS =  4
 
 class PatientDataset(Dataset):
-    def __init__(self, patient_outcomes, patient_creatinine, svs_dir, h5_path, patch_size=256, image_size=64):
+    def __init__(self, data_path, patch_size=256, image_size=64):
         super().__init__()
 
         self.patch_size = patch_size
         self.image_size = image_size
-        self.labels = {'Tubuli': 1, 'Vein': 2, 'Vessel_indeterminate': 2, 'Artery': 3, 'Glomerui': 4}
-        self.h5_path = h5_path
 
-        # Normalise the patient outcomes
-        patient_outcomes["final_outcome"] = patient_outcomes["final_outcome"].apply(normalize_patient_outcomes)
+        self.data_path = data_path
 
-        # Normalise the number of days post transplant
-        patient_outcomes["time_post_transplant"] = patient_outcomes["time post tx of biopsy (days)"].apply(
-            normalize_time_post_transplant)
-
-        # Get the date of biopsy
-        patient_outcomes["date_of_biopsy"] = patient_outcomes["Date of transplantation"] + pd.to_timedelta(
-            patient_outcomes["time post tx of biopsy (days)"], unit='d')
-
-        self.creatinine_avg = {}
-        # Average the creatinine levels between the transplant and biopsy
-        for patient_id, creatinine in tqdm(patient_creatinine.items(), desc="Normalising data"):
-            # Normalise the creatinine values
-            creatinine["creatinine"] = creatinine["Value"].apply(normalize_creatinine)
-
-            # Get the date of the transplant and biopsy
-            transplant_date = \
-            patient_outcomes[patient_outcomes["patient_UUID"] == patient_id]["Date of transplantation"].iloc[0]
-            biopsy_date = patient_outcomes[patient_outcomes["patient_UUID"] == patient_id]["date_of_biopsy"].iloc[0]
-
-            # Get the creatinine values between the transplant and biopsy and average them
-            biopsy_transplant_creatinine = creatinine[(creatinine["Sample Collected Date"] >= transplant_date) & (
-                        creatinine["Sample Collected Date"] <= biopsy_date)]
-            if len(biopsy_transplant_creatinine) > 0:
-                self.creatinine_avg[patient_id] = biopsy_transplant_creatinine["creatinine"].mean()
-            else:
-                self.creatinine_avg[patient_id] = creatinine["creatinine"].mean()
-
-        self.slide_ids = patient_outcomes["slide_UUID"]
-
-        self.patient_outcomes = patient_outcomes
-        self.svs_dir = svs_dir
-
-        self.patch_positions = []
-        self.num_patches = 0
-        for slide_id in tqdm(self.slide_ids, desc="Processing slides"):
-            image = slideio.open_slide(self.svs_dir + slide_id + ".svs", "SVS").get_scene(0)
-
-            # Resize the image to blocks of the patch size
-            small_img = image.read_block(image.rect,
-                                         size=(image.size[0] // self.patch_size, image.size[1] // self.patch_size))
-
-            # Mask out the background
-            img_hs = color.rgb2hsv(small_img)
-            img_hs = np.logical_and(img_hs[:, :, 0] > 0.8, img_hs[:, :, 1] > 0.05)
-
-            # Get the positions of the patches that are not background
-            patch_positions = np.argwhere(img_hs)
-
-            # Scale the positions to the original image size
-            patch_positions = patch_positions * self.patch_size
-
-            self.patch_positions.append(patch_positions)
-            self.num_patches += len(patch_positions)
-
-        # Add the annotated data from the h5file:
-        self.h5_ids = []
-        with h5py.File(self.h5_path, 'r') as h5:
-            for name, cut in h5.items():
-                if any([x in cut.keys() for x in self.labels.keys()]):
-                    if not name.endswith('_0'):  # Omit repeated annotations
-                        self.h5_ids.append(name)
-                else:
-                    print(f'No label data for:{name}')
-        
-	    # Using the 6 slides with the most patches as the test set
-	    unique_slides = Counter([x.split(' ')[0] for x in ids])
-        
-        test_slide = []
-        test_slide_temp = unique_slides.most_common(6)
-        for t in test_slide_temp: test_slide += [t[0]]
-        print(test_slide_temp, test_slide)
-
-        ids_train, ids_test = [], []
-        for x in self.h5_ids:
-            bool_test = False
-            for t in test_slide: 
-                if t in x: bool_test = True 
-            if bool_test:
-                ids_test.append(x)
-            else:
-                ids_train.append(x)
-        print(ids_test)
-        self.h5_ids = ids_train
-        self.ids_test = ids_test
-        print("Images IDs:\n{}".format('\n'.join(self.h5_ids)))
+        # Iterate directory
+        self.slide_ids = []
+        for file in os.listdir(data_path+'/Patches'):
+            if file.endswith('.npy'):
+                self.slide_ids.append(file)  
+        self.num_patches=len(self.slide_ids)
+        print(self.num_patches)
 
     def __len__(self):
-        return 2 * NUM_FLIPS_ROTATIONS * NUM_TRANSLATIONS * self.num_patches
-
-    def index_to_slide(self, index):
-        for i in range(len(self.slide_ids)):
-            if index < len(self.patch_positions[i]):
-                patch_position = self.patch_positions[i][index]
-                return i, (patch_position[1], patch_position[0])
-            else:
-                index -= len(self.patch_positions[i])
+        return NUM_FLIPS_ROTATIONS * NUM_RANDOMCROPS * self.num_patches
 
     def __getitem__(self, original_index):
-        index = original_index // 2
-
-        labelmap = np.zeros((1024, 1024, len(set(self.labels.values()))))
-
-        slide_index, patch_position = self.index_to_slide(index // (NUM_FLIPS_ROTATIONS * NUM_TRANSLATIONS))
-
-        patient_id = self.patient_outcomes.iloc[slide_index]["patient_UUID"]
-
-        # Get data about the patient's outcome
-        num_days_post_transplant = self.patient_outcomes.iloc[slide_index]["time_post_transplant"]
-        final_outcome = self.patient_outcomes.iloc[slide_index]["final_outcome"]
-        if patient_id in self.creatinine_avg:
-            avg_creatinine = self.creatinine_avg[patient_id]
-        else:
-            avg_creatinine = 0
-
-        if original_index % 2 == 0:
-            slide = slideio.open_slide(self.svs_dir + self.slide_ids.iloc[slide_index] + ".svs", "SVS").get_scene(0)
-
-            translation_index = index // NUM_FLIPS_ROTATIONS
-            if translation_index % NUM_TRANSLATIONS == 0:
-                x, y = (patch_position[0], patch_position[1])
-            elif translation_index % NUM_TRANSLATIONS == 1:
-                x, y = (patch_position[0] + self.patch_size // 2, patch_position[1])
-            elif translation_index % NUM_TRANSLATIONS == 2:
-                x, y = (patch_position[0] + self.patch_size // 2, patch_position[1] + self.patch_size // 2)
-            else:
-                x, y = (patch_position[0], patch_position[1] + self.patch_size // 2)
-
-            patch = slide.read_block((x, y, self.patch_size, self.patch_size), size=(self.image_size, self.image_size))
-        else:
-            patch_index = index // (NUM_FLIPS_ROTATIONS * NUM_TRANSLATIONS)
-            patch_index = patch_index % len(self.h5_ids)
-
-            with h5py.File(self.h5_path, 'r') as h5:
-                # Get raw image and combine masks
-                patch = np.array(h5[self.h5_ids[patch_index]].get('rawimage'))
-
-                if patch.shape[2] == 4:  # Get RGB data if images have alpha channel
-                    patch = patch[:, :, :3]
-
-                for labelname in self.labels.keys():
-                    if labelname in h5[self.h5_ids[patch_index]].keys():
-                        mask = np.array(h5[self.h5_ids[patch_index]].get(labelname))
-                        labelmap[mask > 0, self.labels[labelname] - 1] = 1
+    
+        index = original_index
+        patch_index= original_index // (NUM_FLIPS_ROTATIONS * NUM_RANDOMCROPS)      
+        labelmap = np.load(self.data_path+'/Labels/'+self.slide_ids[patch_index][:-4]+'1binary_mask.npy')
+        labelmap = labelmap.reshape((np.shape(labelmap)[0],np.shape(labelmap)[1],1)) #This is actually only necessary if we were to use multiple labels, but keep if for simplicity
+        patch = np.load(self.data_path+'/Patches/'+self.slide_ids[patch_index])
 
         # Convert the patch to a tensor
         patch = torch.from_numpy(patch / 255).permute((2, 0, 1)).float().cuda()
-
         labelmap = torch.from_numpy(labelmap).permute((2, 0, 1)).float().cuda()
-
-        # Convert conditions to tensor
-        conds = torch.tensor([final_outcome, num_days_post_transplant, avg_creatinine]).reshape(1, 3).float().cuda()
+        #Now apply a random crop
+        patch_size = 256
+        img_size = list(patch.size())[1]
+        pos = np.random.uniform(size=2)*(img_size-self.patch_size)
+        patch = T.functional.crop(patch,int(pos[0]),int(pos[1]),self.patch_size,self.patch_size)
+        labelmap = T.functional.crop(labelmap,int(pos[0]),int(pos[1]),self.patch_size,self.patch_size)
 
         # Rotate and flip the patch
         if index % NUM_FLIPS_ROTATIONS == 0:
-            return patch, conds, labelmap
+            return patch, labelmap
         elif index % NUM_FLIPS_ROTATIONS == 1:
-            return patch.flip(2), conds, labelmap.flip(2)
+            return patch.flip(2), labelmap.flip(2)
         elif index % NUM_FLIPS_ROTATIONS == 2:
-            return patch.flip(1), conds, labelmap.flip(1)
+            return patch.flip(1), labelmap.flip(1)
         elif index % NUM_FLIPS_ROTATIONS == 3:
-            return patch.flip(1).flip(2), conds, labelmap.flip(1).flip(2)
+            return patch.flip(1).flip(2), labelmap.flip(1).flip(2)
         elif index % NUM_FLIPS_ROTATIONS == 4:
-            return patch.transpose(1, 2), conds, labelmap.transpose(1, 2)
+            return patch.transpose(1, 2), labelmap.transpose(1, 2)
         elif index % NUM_FLIPS_ROTATIONS == 5:
-            return patch.transpose(1, 2).flip(2), conds, labelmap.transpose(1, 2).flip(2)
+            return patch.transpose(1, 2).flip(2), labelmap.transpose(1, 2).flip(2)
         elif index % NUM_FLIPS_ROTATIONS == 6:
-            return patch.transpose(1, 2).flip(1), conds, labelmap.transpose(1, 2).flip(1)
+            return patch.transpose(1, 2).flip(1), labelmap.transpose(1, 2).flip(1)
         else:
-            return patch.transpose(1, 2).flip(1).flip(2), conds, labelmap.transpose(1, 2).flip(1).flip(2)
+            return patch.transpose(1, 2).flip(1).flip(2), labelmap.transpose(1, 2).flip(1).flip(2)
 
--- train.py
+++ train.py
@@ -32,7 +32,7 @@ def unet_generator(unet_number):
             num_resnet_blocks=3,
             layer_attns=(False, True, True, True),
             layer_cross_attns=(False, True, True, True),
-            cond_images_channels=4,
+            cond_images_channels=1,
         )
 
     if unet_number == 2:
@@ -45,21 +45,9 @@ def unet_generator(unet_number):
             layer_attns=(False, False, False, True),
             layer_cross_attns=(False, False, True, True),
             init_conv_to_final_conv_residual=True,
-            cond_images_channels=4,
+            cond_images_channels=1,
         )
     
-    if unet_number == 3:
-        return Unet(
-            dim=128,
-            cond_dim=512,
-            dim_mults=(1, 2, 4, 8),
-            num_resnet_blocks=(2, 4, 8, 8),
-            memory_efficient=True,
-            layer_attns=False,
-            layer_cross_attns=(False, False, False, True),
-            init_conv_to_final_conv_residual=True,
-            cond_images_channels=4,
-        )
 
     return None
 
@@ -94,15 +82,14 @@ def init_imagen(unet_number):
         unets=(
             unet_generator(1) if unet_number == 1 else FixedNullUnet(),
             unet_generator(2) if unet_number == 2 else FixedNullUnet(lowres_cond=True),
-            unet_generator(3) if unet_number == 3 else FixedNullUnet(lowres_cond=True),
         ),
-        image_sizes=(64, 256, 1024),
+        image_sizes=(64, 256),
         cond_drop_prob=0.1,
-        num_sample_steps=(32, 128, 128),
+        num_sample_steps=(32, 128),
         text_embed_dim=TEXT_EMBED_DIM,
-        random_crop_sizes=(None, None, 256),
+        random_crop_sizes=(None, None),
         sigma_min=0.002,           # min noise level
-        sigma_max=(80, 320, 1280), # max noise level, @crowsonkb recommends double the max noise level for upsampler
+        sigma_max=(80, 320), # max noise level, @crowsonkb recommends double the max noise level for upsampler
     ).cuda()
 
     return imagen
@@ -111,43 +98,19 @@ def init_imagen(unet_number):
 def main():
     args = parse_args()
 
-    # Load the patient outcomes
-    patient_outcomes = pd.read_excel(f'{args.data_path}/outcomes.xlsx', 'Sheet1')
-
-    # Filter any patients that don't have an SVS file
-    slide_ids = [re.sub(r'\.svs', '', os.path.basename(slide)) for slide in glob(f'{args.data_path}/svs/*.svs')]
-    patient_outcomes = patient_outcomes[patient_outcomes['slide_UUID'].isin(slide_ids)]
-
-    # Load all patient creatinine files
-    creatinine_files = glob(f'{args.data_path}/creatinine/*.xlsx')
-    patient_creatinine = {}
-    for file in creatinine_files:
-        df = pd.read_excel(file, 'Sheet1')
-        file_name = os.path.basename(file)
-        patient_id = re.sub(r'\.xlsx$', '', file_name)
-        patient_creatinine[patient_id] = df
-
-    # Filter any creatinine files that don't have an outcome
-    patient_creatinine = {k: v for k, v in patient_creatinine.items() if k in patient_outcomes['patient_UUID'].values}
-
-    print(f'Found {len(patient_outcomes)} patients with SVS files')
-
-    # Load the labelled data from the h5 labelbox download
-    patient_labelled_dir = f'{args.data_path}/results.h5'
-
     # Initialise PatientDataset
-    dataset = PatientDataset(patient_outcomes, patient_creatinine, f'{args.data_path}/svs/', patient_labelled_dir, patch_size=1024, image_size=1024)
+    dataset = PatientDataset(args.data_path, patch_size=256, image_size=1000)
     print(f'Found {len(dataset) // 32} patches')
 
-    for i in [1, 101, 201, 301, 401, 501, 601, 701, 801, 901]:
-        patch, conds, labelmap = dataset[i]
+    for i in [1, 11]:
+        patch, labelmap = dataset[i]
         plt.imshow(patch.permute(1, 2, 0).cpu().numpy())
         for j in range(labelmap.shape[0]):
             data_masked = np.ma.masked_where(labelmap[j].cpu().numpy() == 0, labelmap[j].cpu().numpy())
             plt.imshow(data_masked, alpha=0.5, cmap=matplotlib.colors.ListedColormap(np.random.rand(256, 3)))
         plt.show()
 
-    lowres_image, _, default_labelmap = dataset[101]
+    lowres_image, default_labelmap = dataset[11]
 
     run_name = uuid4()
 
@@ -171,10 +134,8 @@ def main():
 
     if args.unet_number == 1:
         checkpoint_path = args.unet1_checkpoint
-    elif args.unet_number == 2:
-        checkpoint_path = args.unet2_checkpoint
     else:
-        checkpoint_path = args.unet3_checkpoint
+        checkpoint_path = args.unet2_checkpoint
 
     trainer.load(checkpoint_path, noop_if_not_exist=True)
 
@@ -187,11 +148,9 @@ def main():
             print(f'step {trainer.num_steps_taken(args.unet_number)}: unet{args.unet_number} validation loss: {valid_loss}')
 
         if not (i % args.sample_freq) and trainer.is_main:  # is_main makes sure this can run in distributed
-            conds = torch.tensor([0.0, 0.5, 0.2]).reshape(1, 1, 3).float().cuda()
             images = trainer.sample(
                 batch_size=1,
                 return_pil_images=True,
-                text_embeds=conds,
                 start_image_or_video=lowres_image.unsqueeze(0),
                 start_at_unet_number=args.unet_number,
                 stop_at_unet_number=args.unet_number,
@@ -206,8 +165,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--unet1_checkpoint', type=str, default='./unet1_checkpoint.pt', help='Path to checkpoint for unet1 model')
     parser.add_argument('--unet2_checkpoint', type=str, default='./unet2_checkpoint.pt', help='Path to checkpoint for unet2 model')
-    parser.add_argument('--unet3_checkpoint', type=str, default='./unet3_checkpoint.pt', help='Path to checkpoint for unet3 model')
-    parser.add_argument('--unet_number', type=int, choices=range(1, 4), help='Unet to train')
+    parser.add_argument('--unet_number', type=int, choices=range(1, 3), help='Unet to train')
     parser.add_argument('--data_path', type=str, help='Path of training dataset')
     parser.add_argument('--sample_freq', type=int, default=500, help='How many epochs between sampling and checkpoint.pt saves')
     return parser.parse_args()
