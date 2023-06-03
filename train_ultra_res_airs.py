6dad11194ddcbedbc64a9c9c21da41858cee0db7
James Ball <james@ball.sh>
2023-06-03 10:50:29 +0200

    Add training script for AIRS

---
+++ train_ultra_res_airs.py
@@ -0,0 +1,169 @@
+from uuid import uuid4
+
+import matplotlib
+import numpy as np
+import torch
+import argparse
+
+from imagen_pytorch import Unet, ImagenTrainer, Imagen, NullUnet, SRUnet1024, ElucidatedImagen
+from matplotlib import pyplot as plt, cm
+from torch import nn
+from torch.utils.data import Subset, DataLoader
+import torchvision.transforms as T
+
+from ultra_res_airs import AirsDataset
+from train_ultra_res import unet_generator, init_imagen, log_wandb
+import os
+import pandas as pd
+from glob import glob
+from uuid import uuid4
+
+import re
+import gc
+
+
+def main():
+    args = parse_args()
+
+    if args.wandb:
+        import wandb
+    
+    imagen = init_imagen(args.magnification_level, args.unet_number)
+    dl_keywords = ('images',) if args.magnification_level == 0 else ('images', 'cond_images')
+    trainer = ImagenTrainer(
+        imagen=imagen,
+        dl_tuple_output_keywords_names=dl_keywords,
+        fp16=False,
+        # doing this to try and avoid nan
+        max_grad_norm=1,
+    )
+
+    ignore_list = ['christchurch_1003.tif', 'christchurch_1020.tif', 'christchurch_374.tif', 'christchurch_405.tif', 'christchurch_436.tif', 'christchurch_467.tif', 'christchurch_530.tif', 'christchurch_562.tif', 'christchurch_595.tif', 'christchurch_628.tif', 'christchurch_662.tif', 'christchurch_731.tif', 'christchurch_806.tif', 'christchurch_844.tif', 'christchurch_882.tif', 'christchurch_920.tif', 'christchurch_957.tif', 'christchurch_982.tif', 'christchurch_1067.tif', 'christchurch_498.tif', 'christchurch_696.tif']
+
+    train_dataset = AirsDataset(f'{args.data_path}/train/image', ignore_list, args.magnification_level, verbose=True)
+    valid_dataset = AirsDataset(f'{args.data_path}/val/image', ignore_list, args.magnification_level, verbose=True)
+    test_dataset = AirsDataset(f'{args.data_path}/test/image', ignore_list, args.magnification_level, verbose=True)
+
+    trainer.accelerator.print(f'training with dataset of {len(train_dataset)} samples and validating with {len(valid_dataset)} samples')
+
+
+    trainer.add_train_dataset(train_dataset, batch_size=8, num_workers=args.num_workers, shuffle=True)
+    trainer.add_valid_dataset(valid_dataset, batch_size=8, num_workers=args.num_workers, shuffle=True)
+
+    if args.unet_number == 1:
+        checkpoint_path = args.unet1_checkpoint
+    elif args.unet_number == 2:
+        checkpoint_path = args.unet2_checkpoint
+    else:
+        checkpoint_path = args.unet3_checkpoint
+
+    trainer.load(checkpoint_path, noop_if_not_exist=True)
+
+    run_id = None
+
+    if trainer.is_main:
+        if args.wandb:
+            run_id = wandb.util.generate_id()
+        else:
+            run_id = uuid4()
+
+        if args.run_id is not None:
+            run_id = args.run_id
+        trainer.accelerator.print(f"Run ID: {run_id}")
+
+        try:
+            os.makedirs(f"samples/{run_id}")
+        except FileExistsError:
+            pass
+
+        if args.wandb:
+            wandb.init(project=f"training_unet{args.unet_number}", resume=args.resume, id=run_id)
+
+    trainer.accelerator.wait_for_everyone()
+    while True:
+        step_num = trainer.num_steps_taken(args.unet_number)
+        loss = trainer.train_step(unet_number=args.unet_number)
+        trainer.accelerator.print(f'step {step_num}: unet{args.unet_number} loss: {loss}')
+
+        if trainer.is_main:
+            log_wandb(args, step_num, loss)
+
+        if not (step_num % 50):
+            valid_loss = trainer.valid_step(unet_number=args.unet_number)
+            trainer.accelerator.print(f'step {step_num}: unet{args.unet_number} validation loss: {valid_loss}')
+            if trainer.is_main:
+                log_wandb(args, step_num, loss, validation=True)
+
+        if not (step_num % args.save_freq) and step_num > 0:
+            trainer.accelerator.wait_for_everyone()
+            unique_path = f"{re.sub(r'.pt$', '', checkpoint_path)}_{step_num}.pt"
+            trainer.accelerator.print("Saving model...")
+            trainer.save(unique_path)
+            trainer.accelerator.print("Saved model under unique name:")
+            
+
+        if not (step_num % args.sample_freq):
+            trainer.accelerator.wait_for_everyone()
+            trainer.accelerator.print()
+            trainer.accelerator.print("Saving model and sampling")
+
+            if trainer.is_main:
+                lowres_zoomed_image = None
+                rand_zoomed_image = None
+                if args.magnification_level == 0:
+                    lowres_image = test_dataset[0]
+                    rand_image = train_dataset[np.random.randint(len(train_dataset))]
+                else:
+                    lowres_image, lowres_zoomed_image = test_dataset[0]
+                    rand_image, rand_zoomed_image = train_dataset[np.random.randint(len(train_dataset))]
+
+                with torch.no_grad():
+                    if lowres_zoomed_image == None:
+                        images = trainer.sample(
+                            batch_size=2,
+                            return_pil_images=False,
+                            start_image_or_video=torch.stack([lowres_image, rand_image]),
+                            start_at_unet_number=args.unet_number,
+                            stop_at_unet_number=args.unet_number,
+                        )
+                    else:
+                        images = trainer.sample(
+                            batch_size=2,
+                            return_pil_images=False,
+                            start_image_or_video=torch.stack([lowres_image, rand_image]),
+                            start_at_unet_number=args.unet_number,
+                            stop_at_unet_number=args.unet_number,
+                            cond_images=torch.stack([lowres_zoomed_image, rand_zoomed_image]),
+                        )
+
+
+                for index in range(len(images)):
+                    T.ToPILImage()(images[index]).save(f'samples/{run_id}/sample-{step_num}-{run_id}-{index}.png')
+                    if args.wandb:
+                        wandb.log({f"sample{'' if index == 0 else f'-{index}'}": wandb.Image(images[index])})
+    
+            trainer.accelerator.wait_for_everyone()
+            trainer.save(checkpoint_path)
+            trainer.accelerator.print("Finished sampling and saving model!")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--unet1_checkpoint', type=str, default='./unet1_checkpoint.pt', help='Path to checkpoint for unet1 model')
+    parser.add_argument('--unet2_checkpoint', type=str, default='./unet2_checkpoint.pt', help='Path to checkpoint for unet2 model')
+    parser.add_argument('--unet3_checkpoint', type=str, default='./unet3_checkpoint.pt', help='Path to checkpoint for unet3 model')
+    parser.add_argument('--unet_number', type=int, choices=range(1, 4), help='Unet to train')
+    parser.add_argument('--data_path', type=str, help='Path of training dataset')
+    parser.add_argument('--sample_freq', type=int, default=500, help='How many epochs between sampling and checkpoint.pt saves')
+    parser.add_argument('--save_freq', type=int, default=50000, help='How many steps between saving a checkpoint under a unique name')
+    parser.add_argument('--resume', action='store_true', help='Resume previous run using wandb')
+    parser.add_argument("--run_id", type=str, default=None)
+    parser.add_argument("--num_workers", type=int, default=8)
+    parser.add_argument("--magnification_level", type=int, choices=range(0, 3))
+    parser.add_argument('--wandb', action='store_true')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    torch.multiprocessing.set_start_method('spawn')
+    main()
--- ultra_res_airs.py
+++ ultra_res_airs.py
@@ -13,6 +13,7 @@ from tqdm import tqdm
 from skimage import color
 from PIL import Image
 import numpy as np
+from joblib import Parallel, delayed
 import os
 import glob
 
@@ -23,6 +24,95 @@ MAG_LEVEL_SIZES = [10000, 3328, 1024]
 FILL_COLOR = (0, 0, 0)
 
 
+def read_block_mag_zero(image):
+    width = MAG_LEVEL_SIZES[0]
+    height = width
+
+    center_x = width // 2
+    center_y = height // 2
+    zoomed_size = MAG_LEVEL_SIZES[0]
+    x = center_x - zoomed_size // 2
+    y = center_y - zoomed_size // 2
+
+    return read_block(0, x, y, image)
+
+
+# x y is the coordinate of the top-left corner of the patch to read in the overall image
+# mag_level controls the magnification of the patch
+def read_block(mag_level, x, y, image):
+    width = MAG_LEVEL_SIZES[0]
+    height = width
+
+    image_size = MAG_LEVEL_SIZES[mag_level]
+
+    patch_size = MAG_LEVEL_SIZES[2]
+    patch = np.full((patch_size, patch_size, 3), FILL_COLOR, dtype=np.single)
+    patch = torch.from_numpy(patch).permute(2, 0, 1)
+
+    # if coords are negative, cap to 0
+    cropped_x = max(x, 0)
+    cropped_y = max(y, 0)
+
+    # if coords are negative, then the section that is out of bounds
+    # should count towards the image_size so we should trim this off
+    x_trim = max(-x, 0)
+    y_trim = max(-y, 0)
+
+    cropped_width = min(width - cropped_x, image_size - x_trim)
+    cropped_height = min(height - cropped_y, image_size - y_trim)
+
+    patch_width = int(cropped_width * (patch_size / image_size))
+    patch_height = int(cropped_height * (patch_size / image_size))
+
+    cropped_patch = image[:, cropped_y:cropped_y + cropped_height, cropped_x:cropped_x + cropped_width]
+    cropped_patch = F.interpolate(cropped_patch.unsqueeze(0), size=(patch_height, patch_width), mode='nearest').squeeze(0)
+
+    # x and y are relative to the actual kidney image, and we need coordinates
+    # relative to the patch we are returning. x and y define the top-left corner
+    # of the patch, which is coordinate [0,0] so by subtracting x and y from a set
+    # of coordinates, it now is relative to the patch. So we subtract x and y from
+    # cropped_x and cropped_y to get the right coordinates.
+
+    patch_x = cropped_x - x
+    patch_y = cropped_y - y
+
+    # need to multiply by (self.patch_size / image_size) to change coordinates into
+    # the same magnification as the patch, rather than the whole slide.
+    patch_x = int(patch_x * (patch_size / image_size))
+    patch_y = int(patch_y * (patch_size / image_size))
+
+    patch[:, patch_y:patch_y+patch_height, patch_x:patch_x+patch_width] = cropped_patch
+
+    return patch
+
+
+def read_block_and_zoomed(image, y, x, mag_level):
+    image_size = MAG_LEVEL_SIZES[mag_level]
+    center_x = x + image_size // 2
+    center_y = y + image_size // 2
+    zoomed_size = MAG_LEVEL_SIZES[mag_level - 1]
+    zoomed_x = center_x - zoomed_size // 2
+    zoomed_y = center_y - zoomed_size // 2
+
+    patch = read_block(mag_level, x, y, image) 
+    zoomed_patch = read_block(mag_level - 1, zoomed_x, zoomed_y, image)
+
+    return patch, zoomed_patch
+
+
+def process_image(path, mag_level):
+    image = Image.open(path)
+    image = T.ToTensor()(image)
+
+    if mag_level > 0:
+        for y in range(0, MAG_LEVEL_SIZES[0] - MAG_LEVEL_SIZES[2], MAG_LEVEL_SIZES[2] // 2):
+            for x in range(0, MAG_LEVEL_SIZES[0] - MAG_LEVEL_SIZES[2], MAG_LEVEL_SIZES[2] // 2):
+                patch, cond_image = read_block_and_zoomed(image, y, x, mag_level)
+                return patch.to(torch.half), cond_image.to(torch.half)
+    else:
+        return read_block_mag_zero(image).to(torch.half)
+
+
 class AirsDataset(Dataset):
     def __init__(self, image_dir, ignore_list, magnification_level, verbose=False, center_cond=False):
         super().__init__()
@@ -31,97 +121,19 @@ class AirsDataset(Dataset):
         self.center_cond = center_cond
         self.magnification_level = magnification_level
 
-        self.images = []
-        self.images = glob.glob(image_dir + "/*")
-        self.images = [path for path in self.images if os.path.basename(path) not in ignore_list]
+        image_paths = glob.glob(image_dir + "/*")
+        image_paths = [path for path in image_paths if os.path.basename(path) not in ignore_list]
+
+        self.data = Parallel(n_jobs=8)(delayed(process_image)(path, self.magnification_level) for path in tqdm(image_paths))
 
         if verbose:
-            print(f"{len(self.images)} images in dataset")
+            print(f"{len(image_paths)} images in dataset")
+    
 
 
     def __len__(self):
-        return NUM_FLIPS_ROTATIONS * len(self.images)
+        return NUM_FLIPS_ROTATIONS * len(self.data)
 
-
-    def read_block_mag_zero(self, image):
-        width = MAG_LEVEL_SIZES[0]
-        height = width
-
-        center_x = width // 2
-        center_y = height // 2
-        zoomed_size = MAG_LEVEL_SIZES[self.magnification_level]
-        x = center_x - zoomed_size // 2
-        y = center_y - zoomed_size // 2
-
-        return self.read_block(0, x, y, image)
-
-
-    # x y is the coordinate of the top-left corner of the patch to read in the overall image
-    # mag_level controls the magnification of the patch
-    def read_block(self, mag_level, x, y, image):
-        width = MAG_LEVEL_SIZES[0]
-        height = width
-
-        image_size = MAG_LEVEL_SIZES[mag_level]
-
-        patch = np.full((self.patch_size, self.patch_size, 3), FILL_COLOR, dtype=np.single)
-        patch = torch.from_numpy(patch).permute(2, 0, 1)
-
-        # if coords are negative, cap to 0
-        cropped_x = max(x, 0)
-        cropped_y = max(y, 0)
-
-        # if coords are negative, then the section that is out of bounds
-        # should count towards the image_size so we should trim this off
-        x_trim = max(-x, 0)
-        y_trim = max(-y, 0)
-
-        cropped_width = min(width - cropped_x, image_size - x_trim)
-        cropped_height = min(height - cropped_y, image_size - y_trim)
-
-        patch_width = int(cropped_width * (self.patch_size / image_size))
-        patch_height = int(cropped_height * (self.patch_size / image_size))
-
-        cropped_patch = image[:, cropped_y:cropped_y + cropped_height, cropped_x:cropped_x + cropped_width]
-        cropped_patch = F.interpolate(cropped_patch.unsqueeze(0), size=(patch_height, patch_width), mode='nearest').squeeze(0)
-
-        # x and y are relative to the actual kidney image, and we need coordinates
-        # relative to the patch we are returning. x and y define the top-left corner
-        # of the patch, which is coordinate [0,0] so by subtracting x and y from a set
-        # of coordinates, it now is relative to the patch. So we subtract x and y from
-        # cropped_x and cropped_y to get the right coordinates.
-
-        patch_x = cropped_x - x
-        patch_y = cropped_y - y
-
-        # need to multiply by (self.patch_size / image_size) to change coordinates into
-        # the same magnification as the patch, rather than the whole slide.
-        patch_x = int(patch_x * (self.patch_size / image_size))
-        patch_y = int(patch_y * (self.patch_size / image_size))
-
-        patch[:, patch_y:patch_y+patch_height, patch_x:patch_x+patch_width] = cropped_patch
-
-        return patch
-
-
-    def read_block_and_zoomed(self, image):
-        # randomly generate x and y within bounds of the image.
-        # they are valid coordinates at the max magnification to avoid
-        # ever generating black borders at high mag
-        x = np.random.randint(MAG_LEVEL_SIZES[0] - MAG_LEVEL_SIZES[2]) 
-        y = np.random.randint(MAG_LEVEL_SIZES[0] - MAG_LEVEL_SIZES[2]) 
-
-        image_size = MAG_LEVEL_SIZES[self.magnification_level]
-        center_x = x + image_size // 2
-        center_y = y + image_size // 2
-        zoomed_size = MAG_LEVEL_SIZES[self.magnification_level - 1]
-        zoomed_x = center_x - zoomed_size // 2
-        zoomed_y = center_y - zoomed_size // 2
-
-        patch = self.read_block(self.magnification_level, x, y, image) 
-        zoomed_patch = self.read_block(self.magnification_level - 1, zoomed_x, zoomed_y, image)
-
-        return patch, zoomed_patch
 
 
     def flip_rotate_patch(self, index, patch):
@@ -147,13 +159,11 @@ class AirsDataset(Dataset):
         # size of higher mag patch within the zoomed_patch (once a center crop is made)
         patch_width = int(MAG_LEVEL_SIZES[self.magnification_level] * self.patch_size / MAG_LEVEL_SIZES[self.magnification_level - 1])
 
-        image_index = index // NUM_FLIPS_ROTATIONS
-        print(f"opening {self.images[image_index]}")
-        image = Image.open(self.images[image_index])
-        image = T.ToTensor()(image)
+        data_index = index // NUM_FLIPS_ROTATIONS
         
         if self.magnification_level > 0:
-            patch, zoomed_patch = self.read_block_and_zoomed(image)
+            patch, zoomed_patch = self.data[data_index]
+            patch, zoomed_patch = patch.float(), zoomed_patch.float()
             patch = self.flip_rotate_patch(index, patch)
             zoomed_patch = self.flip_rotate_patch(index, zoomed_patch)
             if self.center_cond:
@@ -164,6 +174,6 @@ class AirsDataset(Dataset):
             else:
                 return patch, zoomed_patch
         else:
-            patch = self.read_block_mag_zero(image)
+            patch = self.data[data_index].float()
             return self.flip_rotate_patch(index, patch)
 
